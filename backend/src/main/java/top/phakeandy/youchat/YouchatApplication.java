47c5805a6e0bda47792d08c104e5d980c5c36054
phakeandy <whota0@163.com>
2025-09-16 11:13:03 +0800

    :sparkles: backend initail commit

---
+++ backend/src/main/java/top/phakeandy/youchat/YouchatApplication.java
@@ -0,0 +1,13 @@
+package top.phakeandy.youchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouchatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/top/phakeandy/youchat/YouchatApplicationTests.java
@@ -0,0 +1,13 @@
+package top.phakeandy.youchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
