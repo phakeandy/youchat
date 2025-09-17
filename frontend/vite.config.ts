cf03e6c0243a93686673f36b28c91c95fac2181b
phakeandy <whota0@163.com>
2025-09-17 11:25:33 +0800

    :sparkles: 引入 unplugin-icons 管理工具

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
+import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
 
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
@@ -7,7 +10,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    Components({
+      resolvers: [IconsResolver()],
+    }),
+    Icons(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
